590b8952a8f148cdf8752a4affd956f126e01ccc
pjd199 <89210268+pjd199@users.noreply.github.com>
2022-07-20 16:08:27 +0100

    corrected date issue

--- advent_of_code/utils/solver_status.py
+++ advent_of_code/utils/solver_status.py
@@ -22,7 +22,7 @@ def last_puzzle_date() -> date:
     Returns:
         date: the latest challenge date
     """
-    today = datetime.now(timezone.utc)
+    today = datetime.now(timezone.utc).date()
 
     if today < first_puzzle_date():
         raise RuntimeError(f"Today's date cannot be before {first_puzzle_date()}")
@@ -42,7 +42,7 @@ def puzzle_date_generator() -> Iterable[date]:
     Yields:
         Iterable: _description_
     """
-    today = datetime.now(timezone.utc)
+    today = datetime.now(timezone.utc).date()
 
     if today < first_puzzle_date():
         raise RuntimeError(f"Today's date cannot be before {first_puzzle_date()}")
