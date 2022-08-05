0afaa7d12b19940d7d688e2e6049ff3dd76b566a
pjd199 <89210268+pjd199@users.noreply.github.com>
2022-08-05 08:04:55 +0100

    Updated unit test following changes to display timer

--- tests/unit/test_runner.py
+++ tests/unit/test_runner.py
@@ -22,9 +22,11 @@ def test_runner(capfd: pytest.CaptureFixture[str]) -> None:
     captured = capfd.readouterr()
     lines = [x for x in captured.out.splitlines() if x]
     assert lines[0] == "Solving 'Not Quite Lisp' [2015-01]"
+    assert lines[1] == "Solving part one: (0.00s)"
     assert match(
-        rf"Solved part one: {test_cases['2015']['1'][0]} in \d+.\d\ds", lines[1]
+        rf"Solved part one: {test_cases['2015']['1'][0]} in \d+.\d\ds", lines[2]
     )
+    assert lines[3] == "Solving part two: (0.00s)"
     assert match(
-        rf"Solved part two: {test_cases['2015']['1'][1]} in \d+.\d\ds", lines[2]
+        rf"Solved part two: {test_cases['2015']['1'][1]} in \d+.\d\ds", lines[4]
     )
