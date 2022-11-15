a1ef32016afd11d3fbf8c1b91791d3465e487aa7
pjd199 <89210268+pjd199@users.noreply.github.com>
2022-11-15 12:14:30 +0000

    Solved daily puzzle

---
+++ advent_of_code/utils/ocr.py
@@ -0,0 +1,259 @@
+"""Convert Advent of Code ASCII art.
+
+Forked from https://github.com/bsoyka/advent-of-code-ocr 0.2.0, under MIT License.
+
+"""
+from typing import Sequence, Set, Tuple, Union
+
+
+def convert_coordinates(coordinates: Set[Tuple[int, int]]) -> str:
+    """Convert a set of co-ordinates into letters.
+
+    Args:
+        coordinates (Set[Tuple[int, int]]): the (x,y) pixels of the text
+
+    Returns:
+        str: the OCR'd word
+    """
+    min_x = min(x for x, _ in coordinates)
+    max_x = max(x for x, _ in coordinates)
+    min_y = min(y for _, y in coordinates)
+    max_y = max(y for _, y in coordinates)
+
+    return convert_array(
+        [
+            ["#" if (x, y) in coordinates else "." for x in range(min_x, max_x + 1)]
+            for y in range(min_y, max_y + 1)
+        ]
+    )
+
+
+def convert_array(
+    array: Sequence[Sequence[Union[str, int, bool]]],
+    fill_pixel: Union[str, int, bool] = "#",
+    empty_pixel: Union[str, int, bool] = ".",
+) -> str:
+    """Convert an array of pixels into letters.
+
+    Args:
+        array (Sequence[Sequence[Union[str, int, bool]]]): the input array
+        fill_pixel (Union[str, int, bool], optional): the filled pixel. Defaults to "#".
+        empty_pixel (Union[str, int, bool], optional): the empty pixel. Defaults to ".".
+
+    Raises:
+        ValueError: Raised if the wrong number of rows are found
+        ValueError: Raised if the columns are not all equal length
+
+    Returns:
+        str: _description_
+    """
+    prepared_array = [
+        [
+            "#" if pixel == fill_pixel else "." if pixel == empty_pixel else ""
+            for pixel in line
+        ]
+        for line in array
+    ]
+
+    rows, cols = len(prepared_array), len(prepared_array[0])
+
+    # validate input
+    if any(len(row) != cols for row in prepared_array):
+        raise ValueError("all rows should have the same number of columns")
+
+    if rows not in [6, 10]:
+        raise ValueError("incorrect number of rows (expected 6 or 10)")
+
+    # prepare the settings for 6 or 10 rows
+    if rows == 6:
+        alphabet = ALPHABET_6
+        width = 4
+        padding = 1
+    else:
+        alphabet = ALPHABET_10
+        width = 6
+        padding = 2
+
+    # Convert each letter
+    indices = [slice(start, start + width) for start in range(0, cols, width + padding)]
+    result = [
+        alphabet["\n".join("".join(row[index]) for row in prepared_array)]
+        for index in indices
+    ]
+
+    return "".join(result)
+
+
+ALPHABET_6 = {
+    ".##.\n#..#\n#..#\n####\n#..#\n#..#": "A",
+    "###.\n#..#\n###.\n#..#\n#..#\n###.": "B",
+    ".##.\n#..#\n#...\n#...\n#..#\n.##.": "C",
+    "####\n#...\n###.\n#...\n#...\n####": "E",
+    "####\n#...\n###.\n#...\n#...\n#...": "F",
+    ".##.\n#..#\n#...\n#.##\n#..#\n.###": "G",
+    "#..#\n#..#\n####\n#..#\n#..#\n#..#": "H",
+    ".###\n..#.\n..#.\n..#.\n..#.\n.###": "I",
+    "..##\n...#\n...#\n...#\n#..#\n.##.": "J",
+    "#..#\n#.#.\n##..\n#.#.\n#.#.\n#..#": "K",
+    "#...\n#...\n#...\n#...\n#...\n####": "L",
+    ".##.\n#..#\n#..#\n#..#\n#..#\n.##.": "O",
+    "###.\n#..#\n#..#\n###.\n#...\n#...": "P",
+    "###.\n#..#\n#..#\n###.\n#.#.\n#..#": "R",
+    ".###\n#...\n#...\n.##.\n...#\n###.": "S",
+    "#..#\n#..#\n#..#\n#..#\n#..#\n.##.": "U",
+    "#...\n#...\n.#.#\n..#.\n..#.\n..#.": "Y",
+    "####\n...#\n..#.\n.#..\n#...\n####": "Z",
+}
+
+ALPHABET_10 = {
+    """..##..
+.#..#.
+#....#
+#....#
+#....#
+######
+#....#
+#....#
+#....#
+#....#""": "A",
+    """#####.
+#....#
+#....#
+#....#
+#####.
+#....#
+#....#
+#....#
+#....#
+#####.""": "B",
+    """.####.
+#....#
+#.....
+#.....
+#.....
+#.....
+#.....
+#.....
+#....#
+.####.""": "C",
+    """######
+#.....
+#.....
+#.....
+#####.
+#.....
+#.....
+#.....
+#.....
+######""": "E",
+    """######
+#.....
+#.....
+#.....
+#####.
+#.....
+#.....
+#.....
+#.....
+#.....""": "F",
+    """.####.
+#....#
+#.....
+#.....
+#.....
+#..###
+#....#
+#....#
+#...##
+.###.#""": "G",
+    """#....#
+#....#
+#....#
+#....#
+######
+#....#
+#....#
+#....#
+#....#
+#....#""": "H",
+    """...###
+....#.
+....#.
+....#.
+....#.
+....#.
+....#.
+#...#.
+#...#.
+.###..""": "J",
+    """#....#
+#...#.
+#..#..
+#.#...
+##....
+##....
+#.#...
+#..#..
+#...#.
+#....#""": "K",
+    """#.....
+#.....
+#.....
+#.....
+#.....
+#.....
+#.....
+#.....
+#.....
+######""": "L",
+    """#....#
+##...#
+##...#
+#.#..#
+#.#..#
+#..#.#
+#..#.#
+#...##
+#...##
+#....#""": "N",
+    """#####.
+#....#
+#....#
+#....#
+#####.
+#.....
+#.....
+#.....
+#.....
+#.....""": "P",
+    """#####.
+#....#
+#....#
+#....#
+#####.
+#..#..
+#...#.
+#...#.
+#....#
+#....#""": "R",
+    """#....#
+#....#
+.#..#.
+.#..#.
+..##..
+..##..
+.#..#.
+.#..#.
+#....#
+#....#""": "X",
+    """######
+.....#
+.....#
+....#.
+...#..
+..#...
+.#....
+#.....
+#.....
+######""": "Z",
+}
