fdb466c6a0d121cb4fd12eb4cf02c267500c56e0
pjd199 <89210268+pjd199@users.noreply.github.com>
2023-01-24 14:16:33 +0000

    Solved daily puzzle

--- advent_of_code/year2022/day25.py
+++ advent_of_code/year2022/day25.py
@@ -94,12 +94,12 @@ class Solver(SolverInterface):
         encoded = []
         carry = 0
         encoder = {
-            5: ("0", 1),
-            4: ("-", 1),
-            3: ("=", 1),
-            2: ("2", 0),
-            1: ("1", 0),
             0: ("0", 0),
+            1: ("1", 0),
+            2: ("2", 0),
+            3: ("=", 1),
+            4: ("-", 1),
+            5: ("0", 1),
         }
         for digit in reversed(partial):
             digit += carry
