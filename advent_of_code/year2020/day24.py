2e6480c64c09e0a4c1f8f0d108387a35ef4c6181
pjd199 <89210268+pjd199@users.noreply.github.com>
2023-04-17 09:26:59 +0100

    Solved daily puzzle

---
+++ advent_of_code/year2020/day24.py
@@ -0,0 +1,113 @@
+"""Solves the puzzle for Day 24 of Advent of Code 2020.
+
+Lobby Layout
+
+For puzzle specification and desciption, visit
+https://adventofcode.com/2020/day/24
+"""
+from collections import Counter
+from enum import Enum, unique
+from pathlib import Path
+from re import findall
+from sys import path
+from typing import Callable, Dict, List, Set, Tuple
+
+if __name__ == "__main__":  # pragma: no cover
+    path.append(str(Path(__file__).parent.parent.parent))
+
+from advent_of_code.utils.parser import enum_re, parse_lines
+from advent_of_code.utils.runner import runner
+from advent_of_code.utils.solver_interface import SolverInterface
+
+
+@unique
+class _Direction(Enum):
+    E = "e"
+    SE = "se"
+    SW = "sw"
+    W = "w"
+    NW = "nw"
+    NE = "ne"
+
+
+class Solver(SolverInterface):
+    """Solves the puzzle."""
+
+    YEAR = 2020
+    DAY = 24
+    TITLE = "Lobby Layout"
+
+    def __init__(self, puzzle_input: List[str]) -> None:
+        """Initialise the puzzle and parse the input.
+
+        Args:
+            puzzle_input (List[str]): The lines of the input file
+        """
+        self.input = parse_lines(
+            puzzle_input,
+            (
+                r"[nesw]+",
+                lambda m: [
+                    _Direction(x) for x in findall(rf"({enum_re(_Direction)})", m[0])
+                ],
+            ),
+        )
+
+        # move using the "double co-ordinate" or interlaced system
+        self.move: Dict[_Direction, Callable[[int, int], Tuple[int, int]]] = {
+            _Direction.E: lambda x, y: (x + 2, y),
+            _Direction.SE: lambda x, y: (x + 1, y + 1),
+            _Direction.SW: lambda x, y: (x - 1, y + 1),
+            _Direction.W: lambda x, y: (x - 2, y),
+            _Direction.NW: lambda x, y: (x - 1, y - 1),
+            _Direction.NE: lambda x, y: (x + 1, y - 1),
+        }
+        self.black_tiles: Set[Tuple[int, int]] = set()
+
+    def solve_part_one(self) -> int:
+        """Solve part one of the puzzle.
+
+        Returns:
+            int: the answer
+        """
+        self._lay_floor()
+        return len(self.black_tiles)
+
+    def solve_part_two(self) -> int:
+        """Solve part two of the puzzle.
+
+        Returns:
+            int: the answer
+        """
+        self._lay_floor()
+        black_tiles = set(self.black_tiles)
+        for _ in range(100):
+            neighbours = Counter(
+                self.move[direction](x, y)
+                for (x, y) in black_tiles
+                for direction in _Direction
+            )
+            black_tiles = {
+                (x, y)
+                for (x, y), count in neighbours.items()
+                if count == 2 or (count == 1 and (x, y) in black_tiles)
+            }
+
+        return len(black_tiles)
+
+    def _lay_floor(self) -> None:
+        """Lay the floor."""
+        if not self.black_tiles:
+            self.black_tiles = set()
+            for tile in self.input:
+                x, y = 0, 0
+                for direction in tile:
+                    x, y = self.move[direction](x, y)
+                if (x, y) in self.black_tiles:
+                    self.black_tiles.remove((x, y))
+                else:
+                    self.black_tiles.add((x, y))
+
+
+if __name__ == "__main__":  # pragma: no cover
+    runner(Solver)
---
+++ advent_of_code/year2020/day25.py
@@ -0,0 +1,73 @@
+"""Solves the puzzle for Day 25 of Advent of Code 2020.
+
+Combo Breaker
+
+For puzzle specification and desciption, visit
+https://adventofcode.com/2020/day/25
+"""
+from pathlib import Path
+from sys import path
+from typing import Iterator, List
+
+if __name__ == "__main__":  # pragma: no cover
+    path.append(str(Path(__file__).parent.parent.parent))
+
+from advent_of_code.utils.parser import int_processor, parse_lines
+from advent_of_code.utils.runner import runner
+from advent_of_code.utils.solver_interface import SolverInterface
+
+
+class Solver(SolverInterface):
+    """Solves the puzzle."""
+
+    YEAR = 2020
+    DAY = 25
+    TITLE = "Combo Breaker"
+
+    def __init__(self, puzzle_input: List[str]) -> None:
+        """Initialise the puzzle and parse the input.
+
+        Args:
+            puzzle_input (List[str]): The lines of the input file
+        """
+        self.input = parse_lines(puzzle_input, (r"\d+", int_processor))
+
+    def solve_part_one(self) -> int:
+        """Solve part one of the puzzle.
+
+        Returns:
+            int: the answer
+        """
+
+        def transform(subject_number: int) -> Iterator[int]:
+            value = subject_number
+            while True:
+                value *= subject_number
+                value %= 20201227
+                yield value
+
+        loop_size, value = next(
+            (i + 1, v) for i, v in enumerate(transform(7)) if v in self.input
+        )
+        key = next(k for k in self.input if k != value)
+        return next((v for i, v in enumerate(transform(key)) if i >= loop_size - 1))
+
+    def solve_part_two(self) -> int:
+        """Solve part two of the puzzle.
+
+        Raises:
+            NotImplementedError: always!
+        """
+        raise NotImplementedError("No part two on Christmas Day!!!")
+
+    def solve_all(self) -> List[int]:
+        """Solve the one and only part to this puzzle.
+
+        Returns:
+            List[int]: the result
+        """
+        return [self.solve_part_one()]
+
+
+if __name__ == "__main__":  # pragma: no cover
+    runner(Solver)
